3e1ebd1e50207af36d205280c117e16109aeedb9
fforres <felipe.torressepulveda@gmail.com>
2015-06-15 08:23:07 -0400

    First Commit

---
+++ src/components/TweetList.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+
+class TweetList extends React.Component {
+  render() {
+    return (
+      <ul className="tweetList">
+        <h1>TweetList</h1>
+        // Armar un componente para mostar los tweets
+      </ul>
+    );
+  }
+}
+
+module.exports = TweetList;
